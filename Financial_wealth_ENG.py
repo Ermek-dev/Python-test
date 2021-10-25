32985d3de25690bb75502c89ae93c4cb38fcb61e
Severus <83870911+Ermek-dev@users.noreply.github.com>
2021-10-25 19:09:10 +0600

    Add files via upload

---
+++ Financial_wealth_ENG.py
@@ -0,0 +1,10 @@
+clothes = int(input("Half the monthly cost of buying clothes dollars:"))
+restraunt = int(input("Half of the monthly expenses for visiting restraurants: "))
+present = int(input("half of thr monthly expenses for the little joys in life: "))
+total =clothes+restraunt+present
+print("\nTotal additional costs: ",total,"dollars for month")
+fin_seq = int(input("Total amount financial sequrity: "))
+amount = total+fin_seq
+print("Financial wealth:",amount*12,"dollars for year")
+
+              
